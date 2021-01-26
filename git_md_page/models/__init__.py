f06be93899ea699efd8375dccd09c0f41c3ca99f
Daniel Hubacek <daniel.hubacek@nic.cz>
2021-01-26 16:24:20 +0200

    Ticket #29823 - Create models

---
+++ git_md_page/models/__init__.py
@@ -0,0 +1,7 @@
+"""Models for git_md_page."""
+from .git_plugins import GitRepository, GitTextPluginModel
+
+__all__ = [
+    "GitRepository",
+    "GitTextPluginModel",
+]
---
+++ git_md_page/models/git_plugins.py
@@ -0,0 +1,51 @@
+"""Models for Git plugin."""
+from cms.models import CMSPlugin
+from django.db import models
+from django.utils.crypto import get_random_string
+from django.utils.translation import ugettext as _
+
+
+class GitRepository(models.Model):
+    """Represents a Git repository from which we can pull pages."""
+
+    URL = models.URLField(
+        verbose_name=_("URL for the repository"),
+        help_text=_("https://github.com/owner-name/repository-name.git"),
+    )
+    secret = models.CharField(verbose_name=_("Secret for webhook"), max_length=255, default=get_random_string)
+    branch = models.CharField(
+        verbose_name=_("Source branch"),
+        max_length=63,
+        help_text=_("Source branch is usually either master or main."),
+    )
+
+    class Meta:
+        """Meta class."""
+
+        verbose_name_plural = _("Git repositories")
+
+    def __str__(self):
+        """Understandable representation."""
+        return "Repository at {}".format(self.URL)
+
+
+class GitTextPluginModel(CMSPlugin):
+    """Model for GitTextPlugin."""
+
+    repository = models.ForeignKey(GitRepository, on_delete=models.CASCADE)
+    file = models.CharField(verbose_name=_("Full path to the file in the repository."), max_length=255)
+    content = models.TextField()
+
+    def __str__(self):
+        """Understandable representation."""
+        return "{}".format(self.file)
+
+    def save(self, no_signals=False, *args, **kwargs):
+        """Send a signal to fetch content."""
+        from git_md_page.signals.git_update import repo_update
+
+        signal_sent = kwargs.pop("signal_sent", False)
+        super().save(no_signals=no_signals, *args, **kwargs)
+        if not signal_sent:
+            # We are triggering a save in the signal, so do not fire again
+            repo_update.send(sender=self.__class__, instance=self)
