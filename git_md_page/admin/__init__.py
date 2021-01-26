c45066fe057a6b441e226fc12ac86a94ecd2b9fd
Daniel Hubacek <daniel.hubacek@nic.cz>
2021-01-26 16:24:58 +0200

    Ticket #29823 - Create admin

---
+++ git_md_page/admin/__init__.py
@@ -0,0 +1,6 @@
+"""Administration for git_md_page."""
+from .git_plugin import GitRepositoryAdmin
+
+__all__ = [
+    "GitRepositoryAdmin",
+]
---
+++ git_md_page/admin/git_plugin.py
@@ -0,0 +1,11 @@
+"""Administration for git_md_page."""
+from django.contrib import admin
+
+from git_md_page.models import GitRepository
+
+
+@admin.register(GitRepository)
+class GitRepositoryAdmin(admin.ModelAdmin):
+    """Admin for GitRepository."""
+
+    model = GitRepository
