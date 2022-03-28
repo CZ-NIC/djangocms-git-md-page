c01199cf98f91723436febaa04bf1a5b91ff2996
Zdeněk Böhm <zdenek.bohm@nic.cz>
2022-03-28 10:23:24 +0200

    Fix setup to install templates and translations.

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 # /usr/bin/python3
 """Setup script for djangocms-git-md-page."""
-from setuptools import setup
+from distutils.command.build import build
 
-setup()
+from setuptools import find_packages, setup
+
+
+class CustomBuild(build):
+    sub_commands = [("compile_catalog", lambda x: True)] + build.sub_commands
+
+
+setup(
+    packages=find_packages(exclude=['*.tests']),
+    cmdclass={"build": CustomBuild},
+    setup_requires=["Babel >=2.3"],
+    include_package_data=True,
+)
